5a9e80bf51a1cf398e0d96f02ffadc13330c11ab
SulivanM <139988886+SulivanM@users.noreply.github.com>
2024-05-12 17:07:40 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,4 +5,4 @@ echo "(c) Copyright 2024 - SwitchCompagnie"
 echo "-------------------------------------------------------------------------------------------------"
 echo "Starting DayZ Server & Checking Updates"
 echo "-------------------------------------------------------------------------------------------------"
-./DayZ.sh ${MAP} -log -SteamServerName=$SRV_NAME -Port=$SERVER_PORT
+./DayZServer ${MAP} -log -SteamServerName=$SRV_NAME -Port=$SERVER_PORT
